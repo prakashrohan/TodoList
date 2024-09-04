04fa4e480b3d8b25c9877998fe7de68a714d8c43
Rohan Prakash <pksh.rohan16@gmail.com>
2024-09-04 23:33:07 +0530

    ui

--- TodoList/Models/ItemModel.swift
+++ TodoList/Models/ItemModel.swift
@@ -10,5 +10,5 @@ import Foundation
 struct ItemModel: Identifiable{
     let id: String = UUID().uuidString
     let title : String
-    let isCompleted : Bool
+    var isCompleted : Bool
 }
--- TodoList/Views/ListView.swift
+++ TodoList/Views/ListView.swift
@@ -9,36 +9,43 @@ import SwiftUI
 
 struct ListView: View {
     
-    @State var items : [ItemModel] = [
+    @State var items: [ItemModel] = [
         ItemModel(title: "this is the first", isCompleted: true),
         ItemModel(title: "this is the second", isCompleted: false),
         ItemModel(title: "this is the third", isCompleted: false)
     ]
     
     var body: some View {
-        List{
-            ForEach(items){ item in
-            ListRowView(item: item)
+        List {
+            ForEach(items) { item in
+                ListRowView(item: item)
+                    .onTapGesture {
+                        toggleItemCompletion(item: item)
+                    }
             }
             .onDelete(perform: deleteItem)
             .onMove(perform: moveItem)
         }
-        
         .navigationTitle("Todo List📝")
         .navigationBarItems(
             leading: EditButton(),
             trailing: NavigationLink("Add", destination: AddView())
         )
-        
-    }
-    func deleteItem(indexset : IndexSet){
-        items.remove(atOffsets:indexset)
     }
     
-    func moveItem(from : IndexSet , to : Int){
-        items.move(fromOffsets: from , toOffset: to)
+    func deleteItem(indexSet: IndexSet) {
+        items.remove(atOffsets: indexSet)
     }
     
+    func moveItem(from: IndexSet, to: Int) {
+        items.move(fromOffsets: from, toOffset: to)
+    }
+    
+    func toggleItemCompletion(item: ItemModel) {
+        if let index = items.firstIndex(where: { $0.id == item.id }) {
+            items[index].isCompleted.toggle()
+        }
+    }
 }
 
 #Preview {
